8781e2eec22d9bf304a02b81e296bf09a83bb0b3
Jonathan Moallem <jdmoallem@gmail.com>
2020-03-25 18:45:37 +1100

    Added main and star theme audio tracks

---
+++ Assets/Scripts/DummyScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DummyScript : MonoBehaviour
+{
+    void Start()
+    {
+        SoundGuy.Instance.PlaySound("main_theme", true);
+        StartCoroutine(PlayStarSound());
+    }
+    
+    private IEnumerator PlayStarSound()
+    {
+        yield return new WaitForSeconds(5);
+        SoundGuy.Instance.PlaySound("star_theme", true);
+        yield return new WaitForSeconds(3);
+        SoundGuy.Instance.PlaySound("main_theme", true);
+    }
+}
