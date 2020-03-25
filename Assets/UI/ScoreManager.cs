5290ec4c978299b40b8018443d8fcf4d19a111b7
DaleTGrant <41711729+DaleTGrant@users.noreply.github.com>
2020-03-25 18:45:12 +1100

    Basic UI Complete
    Display UI  complete, alongside point system, coin count system, and timer system

---
+++ Assets/UI/ScoreManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField] private int score;
+
+    public Text scoreText;
+
+    private void Awake()
+    {
+        ResetScore();
+    }
+
+    public void AddPoints(int p)
+    {
+        score += p;
+        UpdateScoreText();
+    }
+
+    public void ResetScore()
+    {
+        score = 0;
+        UpdateScoreText();
+    }
+
+    public void UpdateScoreText()
+    {
+        scoreText.text = ZerosGenerator(score);
+    }
+
+    private string ZerosGenerator(int score)
+    {
+        // depending on the size of the number, place zeros in front of the number before displaying it
+        string zeros = "";
+        for (int i = 5; i > 0; i--)
+        {
+            if (score / Mathf.Pow(10, i) < 1)
+            {
+                zeros += 0;
+            }
+        }
+        return zeros + score.ToString();
+    }
+
+
+}
---
+++ Assets/UI/TimeManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+    private float timer;
+    private float origTimer = 400f;
+    private bool pauseTimer = false;
+
+    public Text timerText;
+
+    private void Awake()
+    {
+        ResetTimer();
+    }
+
+    void Update()
+    {
+        if (!pauseTimer)
+        {
+            DecreaseTimer();
+        }
+    }
+
+    private void DecreaseTimer()
+    {
+        timer -= Time.deltaTime;
+        UpdateTimerText();
+    }
+
+    public void ResetTimer()
+    {
+        timer = origTimer;
+        UpdateTimerText();
+    }
+
+    public void UpdateTimerText()
+    {
+        timerText.text = ZerosGenerator(Mathf.FloorToInt(timer));
+    }
+
+    public void PauseTimer()
+    {
+        pauseTimer = true;
+    }
+
+    public void UnpauseTimer()
+    {
+        pauseTimer = false;
+    }
+
+    private string ZerosGenerator(int score)
+    {
+        // depending on the size of the number, place zeros in front of the number before displaying it
+        string zeros = "";
+        for (int i = 2; i > 0; i--)
+        {
+            if (score / Mathf.Pow(10, i) < 1)
+            {
+                zeros += 0;
+            }
+        }
+        return zeros + score.ToString();
+    }
+}
