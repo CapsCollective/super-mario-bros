0a83fe4c093f9202d1ae5b12c99ee3945ca169d9
DaleTGrant <41711729+DaleTGrant@users.noreply.github.com>
2020-03-25 20:08:17 +1100

    Lives
    Basic Lives Script complete along with UI text for loading screen

---
+++ Assets/UI/Lives.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lives : MonoBehaviour
+{
+    public int lives;
+    private int defaultLives = 3;
+
+    private void Awake()
+    {
+        lives = PlayerPrefs.GetInt("Lives", defaultLives);
+    }
+
+    public void AddLives()
+    {
+
+            lives++;
+        
+    }
+
+    public void LoseLives()
+    {
+        lives--;
+        CheckLives();
+    }
+
+    public void CheckLives()
+    {
+        if(lives < 1)
+        {
+            GameOver();
+        }
+    }
+
+    public void SetLives(int l)
+    {
+        PlayerPrefs.SetInt("Lives", l);
+        PlayerPrefs.Save();
+    }
+
+    public void LevelComplete()
+    {
+        SetLives(lives);
+    }
+
+    private void GameOver()
+    {
+        SetLives(defaultLives);
+
+        // run the gameover from the gameManager
+    }
+}
