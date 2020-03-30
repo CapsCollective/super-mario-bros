cb7a789fd6d3a084cb17d66825fc96f5f9003304
Edisuism <edwardsu230@hotmail.com>
2020-03-30 19:13:14 +1100

    Time stop

--- Assets/Scripts/Mariotransform.cs
+++ Assets/Scripts/Mariotransform.cs
@@ -41,6 +41,7 @@ public class Mariotransform : MonoBehaviour
         if (isTransforming && !inTransform)
         {
             StartCoroutine(coroutineTransform);
+            Time.timeScale = 0;
             isTransforming = false;
             inTransform = true;
             transforms[1].GetComponent<BoxCollider2D>().enabled = false;
@@ -50,6 +51,7 @@ public class Mariotransform : MonoBehaviour
         if (isDamaged && inTransform)
         {
             StartCoroutine(coroutineDamaged);
+            Time.timeScale = 0;
             isDamaged = false;
             inTransform = false;
             transforms[3].GetComponent<BoxCollider2D>().enabled = false;
@@ -61,33 +63,34 @@ public class Mariotransform : MonoBehaviour
     IEnumerator marioTransform()
     {
         sprites[0].enabled = true;
-        yield return new WaitForSeconds(delay);
+        yield return new WaitForSecondsRealtime(delay);
         sprites[0].enabled = false;
         sprites[1].enabled = true;
-        yield return new WaitForSeconds(delay);
+        yield return new WaitForSecondsRealtime(delay);
         sprites[1].enabled = false;
         sprites[0].enabled = true;
-        yield return new WaitForSeconds(delay);
+        yield return new WaitForSecondsRealtime(delay);
         sprites[0].enabled = false;
         sprites[1].enabled = true;
-        yield return new WaitForSeconds(delay);
+        yield return new WaitForSecondsRealtime(delay);
         sprites[1].enabled = false;
         sprites[0].enabled = true;
-        yield return new WaitForSeconds(delay);
+        yield return new WaitForSecondsRealtime(delay);
         sprites[0].enabled = false;
         sprites[1].enabled = true;
-        yield return new WaitForSeconds(delay);
+        yield return new WaitForSecondsRealtime(delay);
         sprites[1].enabled = false;
         sprites[2].enabled = true;
-        yield return new WaitForSeconds(delay);
+        yield return new WaitForSecondsRealtime(delay);
         sprites[2].enabled = false;
         sprites[0].enabled = true;
-        yield return new WaitForSeconds(delay);
+        yield return new WaitForSecondsRealtime(delay);
         sprites[0].enabled = false;
         sprites[1].enabled = true;
-        yield return new WaitForSeconds(delay);
+        yield return new WaitForSecondsRealtime(delay);
         sprites[1].enabled = false;
         sprites[2].enabled = true;
+        Time.timeScale = 1;
         StopCoroutine(coroutineTransform);
     }
 
@@ -95,30 +98,31 @@ public class Mariotransform : MonoBehaviour
     {
         sprites[2].enabled = false;
         sprites[1].enabled = true;
-        yield return new WaitForSeconds(delay);
+        yield return new WaitForSecondsRealtime(delay);
         sprites[1].enabled = false;
         sprites[2].enabled = true;
-        yield return new WaitForSeconds(delay);
+        yield return new WaitForSecondsRealtime(delay);
         sprites[2].enabled = false;
         sprites[1].enabled = true;
-        yield return new WaitForSeconds(delay);
+        yield return new WaitForSecondsRealtime(delay);
         sprites[1].enabled = false;
         sprites[2].enabled = true;
-        yield return new WaitForSeconds(delay);
+        yield return new WaitForSecondsRealtime(delay);
         sprites[2].enabled = false;
         sprites[1].enabled = true;
-        yield return new WaitForSeconds(delay);
+        yield return new WaitForSecondsRealtime(delay);
         sprites[1].enabled = false;
         sprites[0].enabled = true;
-        yield return new WaitForSeconds(delay);
+        yield return new WaitForSecondsRealtime(delay);
         sprites[0].enabled = false;
         sprites[2].enabled = true;
-        yield return new WaitForSeconds(delay);
+        yield return new WaitForSecondsRealtime(delay);
         sprites[2].enabled = false;
         sprites[1].enabled = true;
-        yield return new WaitForSeconds(delay);
+        yield return new WaitForSecondsRealtime(delay);
         sprites[1].enabled = false;
         sprites[0].enabled = true;
+        Time.timeScale = 1;
         StopCoroutine(coroutineDamaged);
     }
 }
