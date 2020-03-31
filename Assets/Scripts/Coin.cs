6b5b407a79ba9e99ae22365d6888fce39b606bf8
Ben Kelly <benkellysoftware@gmail.com>
2020-03-31 13:47:06 +1100

    Coins pickup, pop animation, and sound

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private Vector3 _startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startPos = transform.position;
+        SoundGuy.Instance.PlaySound("smb_coin");
+        StartCoroutine(Run());
+    }
+    
+    private IEnumerator Run()
+    {
+        for (int i = 0; i < 32; i++)
+        {
+            transform.Translate(0,Mathf.Cos(i/10f)/4, 0);
+            yield return null;
+        }
+        //Todo: Increase score and coin count
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/StaticCoin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticCoin : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+        SoundGuy.Instance.PlaySound("smb_coin");
+        //Todo: Increase score and coin counter
+        Destroy(gameObject);
+    }
+}
